66c923db54556df7699437a92ef98494a5f92fca
Joonho Jang <jhjang1005@naver.com>
2021-08-02 23:51:17 +0900

    time 항목이 있으면 __time으로 GOUP BY 하도록 템플릿 및 함수 수정

--- internal/constants/query.go
+++ internal/constants/query.go
@@ -13,13 +13,13 @@ var (
 
 // 일반 쿼리 목록
 const (
-	ServerListSqlQuery = "SELECT server, server_type FROM sys.servers"
+	ServerListQuery    = "SELECT server, server_type FROM sys.servers"
 	TotalSegmentsQuery = `SELECT
-    datasource,
-    SUM("size") AS total_size,
-    CASE WHEN SUM("size") = 0 THEN 0 ELSE SUM("size") / (COUNT(*) FILTER(WHERE "size" > 0)) END AS avg_size,
-    CASE WHEN SUM(num_rows) = 0 THEN 0 ELSE SUM("num_rows") / (COUNT(*) FILTER(WHERE num_rows > 0)) END AS avg_num_rows,
-    COUNT(*) AS num_segments
+  datasource,
+  SUM("size") AS total_size,
+  CASE WHEN SUM("size") = 0 THEN 0 ELSE SUM("size") / (COUNT(*) FILTER(WHERE "size" > 0)) END AS avg_size,
+  CASE WHEN SUM(num_rows) = 0 THEN 0 ELSE SUM("num_rows") / (COUNT(*) FILTER(WHERE num_rows > 0)) END AS avg_num_rows,
+  COUNT(*) AS num_segments
 FROM sys.segments
 GROUP BY 1
 ORDER BY 2 DESC`
@@ -27,8 +27,16 @@ ORDER BY 2 DESC`
 
 // template으로 써야하는 쿼리 목록 (private)
 const (
-	getRollupRatioByDatasourceQueryTpl = "SELECT __time, SUM(`{{ .countColName }}`) * 1.0 / COUNT(*) as rollup_ratio FROM `{{ .datasourceName }}` GROUP BY 1"
-	getDataCntByDatasourceQueryTpl     = "SELECT __time, SUM(`{{ .countColName }}`) as data_cnt FROM `{{ .datasourceName }}` GROUP BY 1"
+	getRollupRatioByDatasourceQueryTpl = `SELECT{{ if .time }}
+  __time,{{ end }}
+  SUM("{{ .countColName }}") * 1.0 / COUNT(*) AS rollup_ratio
+FROM "{{ .datasourceName }}"{{ if .time }}
+GROUP BY 1{{ end }}`
+	getDataCntByDatasourceQueryTpl = `SELECT{{ if .time }}
+  __time,{{ end }}
+  SUM("{{ .countColName }}") AS data_cnt
+FROM "{{ .datasourceName }}"{{ if .time }}
+GROUP BY 1{{ end }}`
 )
 
 func templateQuery(queryName string, queryTpl string, val interface{}) string {
@@ -51,11 +59,19 @@ func templateQuery(queryName string, queryTpl string, val interface{}) string {
 }
 
 // count 컬럼과 데이터소스 명을 입력하여 일자별 rollup 비율을 계산하는 쿼리를 반환합니다.
-func GetRollupRatioByDatasourceQuery(countColName string, datasourceName string) string {
-	return templateQuery("rollupQuery", getRollupRatioByDatasourceQueryTpl, map[string]string{"countColName": countColName, "datasourceName": datasourceName})
+func GetRollupRatioByDatasourceQuery(countColName string, datasourceName string, groupByTime bool) string {
+	var mapping = map[string]string{"countColName": countColName, "datasourceName": datasourceName}
+	if groupByTime {
+		mapping["time"] = "1"
+	}
+	return templateQuery("rollupQuery", getRollupRatioByDatasourceQueryTpl, &mapping)
 }
 
 // count 컬럼과 데이터소스 명을 입력하여 일자별 데이터 건 수 계산하는 쿼리를 반환합니다.
-func GetDataCntByDatasourceQuery(countColName string, datasourceName string) string {
-	return templateQuery("dataCntQuery", getDataCntByDatasourceQueryTpl, map[string]string{"countColName": countColName, "datasourceName": datasourceName})
+func GetDataCntByDatasourceQuery(countColName string, datasourceName string, groupByTime bool) string {
+	var mapping = map[string]string{"countColName": countColName, "datasourceName": datasourceName}
+	if groupByTime {
+		mapping["time"] = "1"
+	}
+	return templateQuery("dataCntQuery", getDataCntByDatasourceQueryTpl, &mapping)
 }
