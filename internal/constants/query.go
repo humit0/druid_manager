ba8eb5a0ca7356fc78e66608ec7b018e0b8e3a83
Joonho Jang <jhjang1005@naver.com>
2021-08-02 23:29:37 +0900

    템플릿 쿼리 함수 생성
    - 롤업 비율 계산 쿼리 적용
    - 데이터 건 수 계산 쿼리 적용

--- internal/constants/query.go
+++ internal/constants/query.go
@@ -7,6 +7,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	entry = logrus.WithFields(logrus.Fields{"type": "query"})
+)
+
 // 일반 쿼리 목록
 const (
 	ServerListSqlQuery = "SELECT server, server_type FROM sys.servers"
@@ -23,26 +27,35 @@ ORDER BY 2 DESC`
 
 // template으로 써야하는 쿼리 목록 (private)
 const (
-	getRollupRatioQueryTpl = "SELECT SUM(`{{ .countColName }}`) * 1.0 / COUNT(*) as rollup_ratio FROM `{{ .datasourceName }}`"
+	getRollupRatioByDatasourceQueryTpl = "SELECT __time, SUM(`{{ .countColName }}`) * 1.0 / COUNT(*) as rollup_ratio FROM `{{ .datasourceName }}` GROUP BY 1"
+	getDataCntByDatasourceQueryTpl     = "SELECT __time, SUM(`{{ .countColName }}`) as data_cnt FROM `{{ .datasourceName }}` GROUP BY 1"
 )
 
-// count 컬럼과 데이터소스 명을 입력하여 rollup 비율을 계산하는 쿼리를 반환합니다.
-func GetRollupRatioQuery(countColName string, datasourceName string) string {
-	tpl := template.New("rollupQuery")
-
-	tpl, err := tpl.Parse(getRollupRatioQueryTpl)
+func templateQuery(queryName string, queryTpl string, val interface{}) string {
+	tpl := template.New(queryName)
+	tpl, err := tpl.Parse(queryTpl)
 
 	if err != nil {
-		logrus.Fatalf("Cannot parse template %v", err)
+		entry.WithField("queryName", queryName).Fatalf("Cannot parse template %v", err)
 	}
 
 	var result bytes.Buffer
 
-	err1 := tpl.Execute(&result, map[string]string{"countColName": countColName, "datasourceName": datasourceName})
+	err1 := tpl.Execute(&result, &val)
 
 	if err1 != nil {
-		logrus.Fatalf("Cannot execute %v", err)
+		entry.WithField("queryName", queryName).Fatalf("Cannot execute template %v", err)
 	}
 
 	return result.String()
+}
+
+// count 컬럼과 데이터소스 명을 입력하여 일자별 rollup 비율을 계산하는 쿼리를 반환합니다.
+func GetRollupRatioByDatasourceQuery(countColName string, datasourceName string) string {
+	return templateQuery("rollupQuery", getRollupRatioByDatasourceQueryTpl, map[string]string{"countColName": countColName, "datasourceName": datasourceName})
+}
+
+// count 컬럼과 데이터소스 명을 입력하여 일자별 데이터 건 수 계산하는 쿼리를 반환합니다.
+func GetDataCntByDatasourceQuery(countColName string, datasourceName string) string {
+	return templateQuery("dataCntQuery", getDataCntByDatasourceQueryTpl, map[string]string{"countColName": countColName, "datasourceName": datasourceName})
 }
